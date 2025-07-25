c7b37430980cc5da816b7a72b914f988f7432bcf
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-07-25 21:34:05 +0545

    Beginning of Banking System in java

---
+++ src/main/java/com/onlineBankingSystem/DatabaseConnection/DatabaseConnection.java
@@ -0,0 +1,14 @@
+package com.onlineBankingSystem.DatabaseConnection;
+
+import java.sql.Connection;
+
+public class DatabaseConnection {
+    public static Connection provideConnection(){
+        Connection con=null;
+        try {
+
+        }catch(){
+
+        }
+    }
+}
