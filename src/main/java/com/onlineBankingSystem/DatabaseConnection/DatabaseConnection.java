1aa9621865ffe03406fe34fad4f2840b824bad71
Suresh_Thakur <sureshthakur2080@gmail.com>
2025-07-29 00:03:44 +0545

    databse is connected to the banking system

--- src/main/java/com/onlineBankingSystem/DatabaseConnection/DatabaseConnection.java
+++ src/main/java/com/onlineBankingSystem/DatabaseConnection/DatabaseConnection.java
@@ -1,15 +1,27 @@
 package com.onlineBankingSystem.DatabaseConnection;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static java.lang.Class.forName;
 
 public class DatabaseConnection {
     public static Connection provideConnection(){
         Connection con=null;
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-        }catch(){
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        String url="jdbc:mysql://localhost:3306/bankingsystem";
+        try{
+            con= DriverManager.getConnection(url,"root","9821771258@999");
+        }catch(Exception e){
+            System.out.println("Exception occur in database");
 
         }
+
         return con;
     }
 }
